283540c8ae3841a40f90505b9d654f83c4211f0c
pelmyaai <mhr2154@gmail.com>
2026-05-24 12:48:47 +0900

    Create my-pocketrisu.sh

---
+++ my-pocketrisu.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+echo "[PocketRisu] installer start"
+
+cd ~
+
+if ! command -v curl >/dev/null 2>&1; then
+  sudo apt update
+  sudo apt install -y curl
+fi
+
+curl -L -o PocketRisu.tar.gz https://github.com/PocketRisu/PocketRisu/releases/download/v1.6.0/PocketRisu-v1.6.0-linux-x64.tar.gz
+
+rm -rf pocketrisu
+mkdir -p pocketrisu
+
+tar -xzf PocketRisu.tar.gz -C pocketrisu
+
+cd pocketrisu
+
+echo "[PocketRisu] server start"
+bash start.sh
